85fcd4232690e19aca3770b924539fcdb053a442
rokatyy <erogatova@gmail.com>
2020-03-17 21:28:40 +0300

    new very easy reverse

---
+++ Reverse/crackme/file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+char secret_array[16];
+scanf("%s",secret_array);
+if (strcmp(secret_array,"flag{e4sy_C_language}")==0){printf("Congratulations!\n");}
+else {
+printf("Ne polushilos, ne fortanulo\n");}
+return 0;
+
+}
