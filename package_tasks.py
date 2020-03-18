3f6b846723c4be65c3a59d8d105501e473e4cea7
krikun98 <krikun98@gmail.com>
2020-03-18 11:45:33 +0300

    task_pack refactor

--- package_tasks.py
+++ package_tasks.py
@@ -10,17 +10,17 @@ def package():
     task_groups = ['Crypto', 'Osint', 'Reverse', 'Stegano']
     reverse_index = re.compile(r"(\.c|\.cpp)")
     for task_group in task_groups:
-        names = os.listdir('{:}'.format(task_group))
+        names = os.listdir(f'{task_group}')
         for name in names:
-            name_dir = '{:}/{:}'.format(task_group, name)
-            new_name_dir = '{:}/{:}/{:}'.format(task_pack, task_group, name)
+            name_dir = f'{task_group}/{name}'
+            new_name_dir = f'{task_pack}/{name_dir}'
             os.makedirs(new_name_dir, exist_ok=True)
             files = os.listdir(name_dir)
             for file in files:
                 if file in ['hash.txt', 'secret.txt', 'value.txt'] or 'hidden' in file or reverse_index.search(file):
                     continue
-                file_address = '{:}/{:}'.format(name_dir, file)
-                new_file_address = '{:}/{:}'.format(new_name_dir, file)
+                file_address = f'{name_dir}/{file}'
+                new_file_address = f'{new_name_dir}/{file}'
                 copyfile(file_address, new_file_address)
     make_archive(task_pack, 'tar', task_pack)
     rmtree(task_pack)
